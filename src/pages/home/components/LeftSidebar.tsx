d65affa58fd1fb3a6c6fb1f727c3ff1c290086d6
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-05-06 01:15:26 +0300

    feat: finish UI for left sidebar

--- src/pages/home/components/LeftSidebar.tsx
+++ src/pages/home/components/LeftSidebar.tsx
@@ -3,9 +3,54 @@
 
 //@ts-ignore
 import { jsx, css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { Light } from "../../../styles/colors";
+import Logo from "./Logo";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${Light.primary};
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px;
+`;
+
+const Tab = styled.button`
+  font-family: inherit;
+  font-size: 24px;
+  text-align: center;
+  border: none;
+  width: 100%;
+  padding: 8px 16px;
+  background-color: ${Light.primary};
+  color: ${Light.textColor};
+  border-radius: 12px;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    background-color: ${Light.hover};
+    color: ${Light.primary};
+  }
+`;
 
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  return (
+    <Container>
+      <Logo />
+      <div
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          gap: "5px",
+        }}
+      >
+        <Tab>Browse</Tab>
+        <Tab>Favorites</Tab>
+      </div>
+      <div></div>
+    </Container>
+  );
 };
 
 export default LeftSidebar;
---
+++ src/pages/home/components/Logo.tsx
@@ -0,0 +1,18 @@
+import { Light } from "../../../styles/colors";
+
+function Logo() {
+  return (
+    <div
+      css={{
+        fontFamily: "Z003",
+        padding: "4px 8px",
+        color: Light.textColor,
+        fontSize: "32px",
+      }}
+    >
+      Zmarie
+    </div>
+  );
+}
+
+export default Logo;
