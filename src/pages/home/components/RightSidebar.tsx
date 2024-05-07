746d7eb5e510a07914f03d4fbd0a92d89dbc1877
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-05-07 22:47:05 +0300

    feat: finish right sidebar UI

--- src/pages/home/components/RightSidebar.tsx
+++ src/pages/home/components/RightSidebar.tsx
@@ -3,9 +3,47 @@
 
 //@ts-ignore
 import { jsx, css } from "@emotion/react";
+import styled from "@emotion/styled";
+import SongTile from "../../../common/components/SongTile";
+import { Light } from "../../../styles/colors";
+import Heading from "../../../common/components/Heading";
+
+const Container = styled.div`
+  padding: 10px;
+  color: ${Light.textColor};
+  height: 100dvh;
+  overflow-y: auto;
+`;
+
+const SongList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 
 function RightSidebar() {
-  return <div>RightSidebar</div>;
+  return (
+    <Container>
+      <div css={{ height: "100px" }}></div>
+      <Heading title="Now playing" />
+      <SongList>
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+        <SongTile />
+      </SongList>
+    </Container>
+  );
 }
 
 export default RightSidebar;
