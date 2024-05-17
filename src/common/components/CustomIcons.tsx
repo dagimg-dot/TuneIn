b5437b2caa5cef86ee3e81ce942913d29d5f2f10
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-05-17 23:47:26 +0300

    fix: remove ambiguous attributes

--- src/common/components/CustomIcons.tsx
+++ src/common/components/CustomIcons.tsx
@@ -78,11 +78,9 @@ export function DurationIcon() {
       width="20"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-1-7.59V4h2v5.59l3.95 3.95-1.41 1.41L9 10.41z"></path>
