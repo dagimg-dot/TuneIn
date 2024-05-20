7c244c8b3eec5a7ab6fd3c39f09b050ec15285ae
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-05-21 01:34:11 +0300

    feat: update color object

--- src/shared/styles/colors.ts
+++ src/shared/styles/colors.ts
@@ -3,4 +3,8 @@ export const Light = {
   secondary: "rgb(27,13,0)",
   hover: "#f5f5f5",
   textColor: "#eee",
+  tileHover: "#4F4E4E",
+  playerRange: "#20ACFB",
+  playerBlur: "#283036",
+  textSecondary: "#bbb",
 };
