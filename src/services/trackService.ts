2570e9b5ddae251a9dac122755e8d035d17792d7
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-05-26 18:28:30 +0300

    refactor: change backend of tracks to LS

--- src/services/trackService.ts
+++ src/services/trackService.ts
@@ -1,28 +1,22 @@
-// all crud operations for track
-
-import TuneClient from "./TuneClient";
 import { Track } from "../shared/types";
+import LSClient from "./LSClient";
 
-export const getTracks = async (): Promise<Track[]> => {
-  return await TuneClient.get("/tracks");
+export const getTracks = async (): Promise<Track | Track[]> => {
+  return await LSClient.get("/tracks");
 };
 
-export const getTrack = async (id: string): Promise<Track> => {
-  return await TuneClient.get(`/tracks/${id}`);
+export const getTrack = async (id: string): Promise<Track | Track[]> => {
+  return await LSClient.get(`/tracks/${id}`);
 };
 
 export const addTrack = async (track: Track): Promise<Track> => {
-  return await TuneClient.post("/tracks", track);
+  return await LSClient.post("/tracks", track);
 };
 
 export const updateTrack = async (track: Track): Promise<Track> => {
-  return await TuneClient.put(`/tracks/${track.id}`, track);
+  return await LSClient.put(`/tracks/${track.id}`, track);
 };
 
 export const deleteTrack = async (id: string): Promise<void> => {
-  return await TuneClient.delete(`/tracks/${id}`);
-};
-
-export const searchTracks = async (query: string): Promise<Track[]> => {
-  return await TuneClient.get(`/tracks/search?q=${query}`);
+  return await LSClient.delete(`/tracks/${id}`);
 };
